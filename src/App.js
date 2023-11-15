df6e2e35525bdc4c350641bfae9f3c8a3c00dde5
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-11-15 11:22:19 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { ReactDOM } from "react-dom/client";
+import React from "react";
+
+
+const App = () => {
+    return(
+        <div className="App">
+
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App/>);
